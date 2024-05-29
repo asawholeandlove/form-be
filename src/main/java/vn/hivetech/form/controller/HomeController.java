fe2b48d25791b6c95efdaf1e0e6b913711a01798
dangthaii <thaidv@hivetech.vn>
2024-05-29 16:49:24 +0700

    init project

---
+++ src/main/java/vn/hivetech/form/controller/HomeController.java
@@ -0,0 +1,13 @@
+package vn.hivetech.form.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  
+  @GetMapping("/")
+  public String home() {
+    return "Hello World!";
+  }
+}
