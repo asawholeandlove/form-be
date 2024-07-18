904fe992beb7f616639e29a49ab5604e3228ea39
dangthaii <thaidv@hivetech.vn>
2024-07-18 10:09:14 +0700

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,13 +6,23 @@ import { User } from "./schemas/user.schema";
 import { Model } from "mongoose";
 import { omit } from "lodash";
 import { getHashPassword } from "src/utils/auths.util";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private configService: ConfigService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
     const { username } = createUserDto;
+    const code = this.configService.get("REGISTER_CODE") || "hive";
+    if (createUserDto.code !== code) {
+      throw new BadRequestException(
+        `Mã code không hợp lệ. Vui lòng kiểm tra lại.`,
+      );
+    }
     const isExist = await this.userModel.findOne({ username });
     if (isExist) {
       throw new BadRequestException(
